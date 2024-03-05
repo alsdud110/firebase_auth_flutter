271e06584237ca635f526cff61af812fe952b60e
alsdud110 <alsdud110@naver.cpm>
2024-03-05 22:39:53 +0900

    코멘트 기능 추가

---
+++ lib/components/comment.dart
@@ -0,0 +1,53 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Comment extends StatelessWidget {
+  final String commentUser;
+  final String commentText;
+  final Timestamp commentTime;
+  const Comment(
+      {super.key,
+      required this.commentUser,
+      required this.commentText,
+      required this.commentTime});
+
+  @override
+  Widget build(BuildContext context) {
+    final commentDateTime = commentTime.toDate();
+
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.circular(6),
+      ),
+      padding: const EdgeInsets.only(left: 15, top: 15, bottom: 15),
+      margin: const EdgeInsets.only(top: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(commentText),
+          const SizedBox(
+            height: 6,
+          ),
+          Row(
+            children: [
+              Text(
+                commentUser,
+                style: TextStyle(color: Colors.grey[400]),
+              ),
+              Text(
+                " • ",
+                style: TextStyle(color: Colors.grey[400]),
+              ),
+              Text(
+                DateFormat("MM/dd HH:MM").format(commentDateTime),
+                style: TextStyle(color: Colors.grey[400]),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/post.dart
+++ lib/pages/post.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_post/components/comment.dart';
+import 'package:firebase_post/components/comment_button.dart';
 import 'package:firebase_post/components/like_button.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -25,6 +27,7 @@ class Post extends StatefulWidget {
 class _PostState extends State<Post> {
   final currentUser = FirebaseAuth.instance.currentUser!;
   bool isLiked = false;
+  final _editingTextController = TextEditingController();
 
   @override
   void initState() {
@@ -52,6 +55,65 @@ class _PostState extends State<Post> {
     }
   }
 
+  void addComment(String comment) async {
+    await FirebaseFirestore.instance
+        .collection("User Posts")
+        .doc(widget.postId)
+        .collection("Comments")
+        .add(
+      {
+        "CommentText": comment,
+        "CommentedBy": currentUser.email,
+        "CommentTime": DateTime.now(),
+      },
+    );
+  }
+
+  void showCommentDialog() {
+    _editingTextController.clear();
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        backgroundColor: Colors.grey[900],
+        title: const Text(
+          "Add Comment",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        content: TextField(
+          controller: _editingTextController,
+          style: const TextStyle(color: Colors.white),
+          decoration: const InputDecoration(
+            hintText: "Add a Comment",
+            hintStyle: TextStyle(color: Colors.white),
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text(
+              "Cancel",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              addComment(_editingTextController.text);
+              Navigator.pop(context);
+            },
+            child: const Text(
+              "Save",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final dateTime = widget.date.toDate();
@@ -62,25 +124,16 @@ class _PostState extends State<Post> {
         borderRadius: BorderRadius.circular(8),
       ),
       margin: const EdgeInsets.only(top: 25, left: 25, right: 25),
-      padding: const EdgeInsets.all(25),
-      child: Row(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Column(
-            children: [
-              LikeButton(isLiked: isLiked, onTap: toggleLike),
-              Text(
-                widget.likes.length.toString(),
-                style: const TextStyle(
-                  color: Colors.grey,
-                ),
-              ),
-            ],
-          ),
+          Text(widget.text),
           const SizedBox(
-            width: 20,
+            height: 16,
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(
                 widget.currentUser,
@@ -91,18 +144,81 @@ class _PostState extends State<Post> {
               const SizedBox(
                 height: 10,
               ),
-              Text(widget.text),
+              Text(
+                DateFormat("MM/dd HH:MM").format(dateTime),
+                style: TextStyle(
+                  color: Colors.grey[500],
+                ),
+              ),
             ],
           ),
-          const Spacer(),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 25),
-            child: Text(
-              DateFormat("MM/dd HH:MM").format(dateTime),
-              style: TextStyle(
-                color: Colors.grey[500],
+          const SizedBox(
+            height: 8,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  LikeButton(isLiked: isLiked, onTap: toggleLike),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    widget.likes.length.toString(),
+                    style: const TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
               ),
-            ),
+              const SizedBox(
+                width: 10,
+              ),
+              Column(
+                children: [
+                  CommentButton(onTap: showCommentDialog),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  const Text(
+                    "0",
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          StreamBuilder(
+            stream: FirebaseFirestore.instance
+                .collection("User Posts")
+                .doc(widget.postId)
+                .collection("Comments")
+                .orderBy("CommentTime", descending: true)
+                .snapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+
+              return ListView(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                children: snapshot.data!.docs
+                    .map(
+                      (e) => Comment(
+                        commentText: e["CommentText"],
+                        commentTime: e["CommentTime"],
+                        commentUser: e["CommentedBy"],
+                      ),
+                    )
+                    .toList(),
+              );
+            },
           ),
         ],
       ),
